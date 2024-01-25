a3b3bb69978a64c7154a2948ad6f33f8d41d786b
amaalyy <amal.elzowawi@gmail.com>
2024-01-25 21:36:05 +0100

    Star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,13 @@ const request = require('request');
 
 const id = process.argv[2];
 
-request('https://swapi-api.hbtn.io/api/films/' + id, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
+request(
+  'https://swapi-api.hbtn.io/api/films/' + id,
+  (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).title);
+    }
   }
-});
-
+);
