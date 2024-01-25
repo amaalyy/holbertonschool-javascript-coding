7a989558aa0785efac3a21f04d350fa445c8b6ba
amaalyy <amal.elzowawi@gmail.com>
2024-01-25 15:50:14 +0100

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const fileConent = process.argv[3];
 
-f.writeFile(filePath, fileConent, 'utf-8', (err) => {
+fs.writeFile(filePath, fileConent, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
