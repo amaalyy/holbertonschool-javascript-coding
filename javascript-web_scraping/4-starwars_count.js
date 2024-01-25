379225bbf28d60fffd818b9cbb973fb40d17ad50
amaalyy <amal.elzowawi@gmail.com>
2024-01-25 22:00:14 +0100

    Star wars Wedge Antilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/opt/homebrew/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let ct = 0;
+	for (const film of films) {
+		for (const character of film.characters) {
+			if (character.includes('18')) {
+				ct++
+			}
+		}
+	}
+	console.log(ct)
+  }
+});
