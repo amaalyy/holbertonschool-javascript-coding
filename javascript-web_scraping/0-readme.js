dd40bc169e533abc624c38e2c7f2095b3029cacd
amaalyy <amal.elzowawi@gmail.com>
2024-01-25 15:23:17 +0100

    Readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/opt/homebrew/bin/node
+const f = require('fs');
+const argv = process.argv[2];
+
+f.readFile(argv, 'utf-8', function (error, data) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
