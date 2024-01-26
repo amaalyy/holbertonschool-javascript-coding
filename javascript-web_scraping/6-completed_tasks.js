0a8e8ed13ffd18a53ee927a6ea976eb93c508917
amaalyy <amal.elzowawi@gmail.com>
2024-01-26 23:46:02 +0100

    6. How many completed?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const url = process.argv[2];
+
 request(url, (error, response, body) => {
-    const todo = JSON.parse(body);
-    const completed = {};
-    todo.forEach((item) => {
-        if (item.completed) {
-            if (completed[item.userId]) {
-                completed[item.userId]++;
-            }else {
-                completed[item.userId] = 1;
-            }
-        }
+  const todo = JSON.parse(body);
+  const completed = {};
+
+  todo.forEach((item) => {
+    if (item.completed) {
+      if (completed[item.userId]) {
+        completed[item.userId]++;
+      } else {
+        completed[item.userId] = 1;
       }
-    });
-    console.log(completed);
-  
+    }
+  });
+  console.log(completed);
 });
